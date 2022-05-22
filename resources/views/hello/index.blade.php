e96d7f2e30314008345897a9d455ef8e19c20f86
yuu-sa <kamechan456@gmail.com>
2022-05-23 00:33:15 +0900

    P84

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,15 +9,17 @@
   </head>
   <body>
     <h1>Blaede/Index</h1>
-    <p>&#064;forディレクティブの例</p>
-    @foreach ($data as $item)
-    @if ($loop->first)
-    <p>※データ一覧</p><ul>
-    @endif
-    <li>No,{{$loop->iteration}}. {{$item}}</li>
-    @if ($loop->last)
-    </ul><p>ーーここまで</p>
-    @endif
-    @endforeach
+    <p>&#064;whileディレクティブの例</p>
+    <ol>
+    @php
+    $counter = 0;
+    @endphp
+    @while ($counter < count($data))
+    <li>{{$data[$counter]}}</li>
+    @php
+    $counter++;
+    @endphp
+    @endwhile
+    </ol>
   </body>
 </html>
