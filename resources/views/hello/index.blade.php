f2dec48e4ca38932afcfb04e3ad68d590b0ddfc8
yuu-sa <kamechan456@gmail.com>
2022-05-22 18:35:05 +0900

    P80

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,15 +9,11 @@
 </head>
 <body>
     <h1>Blaede/Index</h1>
-    @if ($msg !='')
-    <p>こんにちは、{{$msg}}さん。</p>
-    @else
-    <p>何か書いてください。</p>
-    @endif
-    <form method="POST" action="/hello">
-      @csrf
-      <input type="text" name="msg">
-      <input type="submit">
-    </form>
+    <p>&#064;foreachディレクティブの例</p>
+    <ol>
+    @foreach($data as $item)
+    <li>{{$item}}
+    @endforeach
+  </ol>
   </body>
 </html>
