e658e0d1928ca6e97ae609ad1ef3e2d0ddc5503d
yuu-sa <kamechan456@gmail.com>
2022-05-25 14:55:17 +0900

    P93

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,17 @@
 @section('content')
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます。</p>
+
+    @component('components.message')
+      @slot('msg_title')
+      CAUTION!
+      @endslot
+
+      @slot('msg_content')
+      これはメッセージの表示です。
+      @endslot
+    @endcomponent
+    
 @endsection
 
 @section('footer')
