2a5abb3844e7bb831b1ad62b55818f32c0701a99
yuu-sa <kamechan456@gmail.com>
2022-05-25 14:18:16 +0900

    P90

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,25 +1,17 @@
-<html>
-  <head>
-    <title>Hello/index</title>
-    <style>
-    body {font-size:16pt; color:#999; }
-    h1 { font-size:-50pt; text-align:right; color:#f6f6f6;
-       margin:-20px 0px -30px 0px; letter-spacing: -4pt; }
-    </style>
-  </head>
-  <body>
-    <h1>Blaede/Index</h1>
-    <p>&#064;whileディレクティブの例</p>
-    <ol>
-    @php
-    $counter = 0;
-    @endphp
-    @while ($counter < count($data))
-    <li>{{$data[$counter]}}</li>
-    @php
-    $counter++;
-    @endphp
-    @endwhile
-    </ol>
-  </body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
